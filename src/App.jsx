fe2cf20fd1efb1d3615517b5411c01a6b84cc9db
Nani1948 <nandhinikaluvakollu18@gmail.com>
2026-09-05 17:49:51 +0530

    Set up App and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import{BrowserRouter,Routes,Route}from "react-router-dom";
+import Header from "./components/Header.jsx";
+import "./App.css";
+import Home from "./pages/Home.jsx";
+import BrowseBooks from "./pages/BrowseBooks.jsx";
+import BookDetails from "./pages/BookDetails.jsx";
+import AddBook from "./pages/AddBook.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      {/*Home Page*/}
+      <Route path="/" element={<PageWithHeader>
+        <Home/>
+      </PageWithHeader>}/>
+
+      {/*Books Page*/}
+      <Route 
+      path="/books" 
+      element={   <PageWithHeader>
+                            <BrowseBooks />
+                        </PageWithHeader>}/>
+      
+      {/*Browse Categories*/}
+      <Route 
+      path="/books/:category" 
+      element={   <PageWithHeader>
+                            <BrowseBooks />
+                        </PageWithHeader>}/>
+
+      {/*Book Details*/}
+      <Route
+      path="/books/:category/:id"
+      element={<PageWithHeader><BookDetails/></PageWithHeader>}/>
+
+      {/*Add Book*/}
+      <Route
+      path="/add-book"
+      element={<PageWithHeader><AddBook/></PageWithHeader>}/>
+
+       {/*404 Page*/}
+       <Route 
+      path="*"
+      element={<NotFound/>}/>
+  
+
+    </Routes>
+    </BrowserRouter> );
+}
+// Layout with Header
+function PageWithHeader({ children }) {
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    );
 }
 
-export default App
+export default App;
